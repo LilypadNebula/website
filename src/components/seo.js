3792ab494b6cbd95e8336870fe54a5c2f3a0a976
Lily <lilypadnebula@gmail.com>
2019-02-10 14:25:10 -0500

    Add og image, update to stable react

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,43 +3,51 @@ import Helmet from 'react-helmet'
 
 const SEO = props => (
   <Helmet
-  htmlAttributes={{lang:'en'}}
-  title={props.title}
-  titleTemplate={`%s | Lilypad Nebula`}
-  meta={[
-    {
-      name: `description`,
-      content: props.description,
-    },
-    {
-      property: `og:title`,
-      content: props.title,
-    },
-    {
-      property: `og:description`,
-      content: props.description,
-    },
-    {
-      property: `og:type`,
-      content: `website`,
-    },
-    {
-      name: `twitter:card`,
-      content: `summary`,
-    },
-    {
-      name: `twitter:creator`,
-      content: '@lilypadnebula',
-    },
-    {
-      name: `twitter:title`,
-      content: props.title,
-    },
-    {
-      name: `twitter:description`,
-      content: props.description,
-    }
-  ]}
+    htmlAttributes={{ lang: 'en' }}
+    title={props.title}
+    titleTemplate={`%s | Lilypad Nebula`}
+    meta={[
+      {
+        name: `description`,
+        content: props.description,
+      },
+      {
+        property: `og:title`,
+        content: props.title,
+      },
+      {
+        property: `og:description`,
+        content: props.description,
+      },
+      {
+        property: `og:type`,
+        content: `website`,
+      },
+      {
+        property: 'twitter:image',
+        content: '/chibi_no_bg.png',
+      },
+      {
+        property: 'og:image',
+        content: '/chibi_no_bg.png',
+      },
+      {
+        name: `twitter:card`,
+        content: `summary`,
+      },
+      {
+        name: `twitter:creator`,
+        content: '@lilypadnebula',
+      },
+      {
+        name: `twitter:title`,
+        content: props.title,
+      },
+      {
+        name: `twitter:description`,
+        content: props.description,
+      },
+    ]}
   />
 )
 
