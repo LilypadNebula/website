10579ba25805b4d8e64cb276a15fdc60536bcfc4
Lily <lilypadnebula@gmail.com>
2019-02-17 18:01:29 -0500

    Remove Fair Wanderings

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -6,40 +6,33 @@ import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
 const SchedulePage = () => (
-      <Layout>
-        <SEO title="Schedule" description="Find out when I'm doing things, like streams and such"/>
-        <FadingContent className="flex flex-col items-center text-center">
-          <p className="text-2xl font-montAlt">
-            Below are the things that should occur weekly barring any unforeseen
-            stuff. I'll update this when something with a schedule happens,
-            otherwise Twitter is where I'll announce anything that randomly
-            comes up
-          </p>
-          <EventList
-            title="Recurring"
-            events={[
-              {
-                weekday: 'Sunday',
-                time: '1 PM EST',
-                name: 'Winds of Chaos, A Warhammer Fantasy 4e Game',
-                link: {
-                  url: 'https://twitch.tv/welcomepartyrpg',
-                  text: 'Welcome To The Party Twitch',
-                },
-              },
-              {
-                weekday: 'Wednesday',
-                time: '8 PM EST',
-                name: 'Fair Wanderings, A D&D 5e game',
-                link: {
-                  url: 'https://twitch.tv/lilypadnebula',
-                  text: 'My Twitch',
-                },
-              },
-            ]}
-          />
-        </FadingContent>
-      </Layout>
-    )
+  <Layout>
+    <SEO
+      title="Schedule"
+      description="Find out when I'm doing things, like streams and such"
+    />
+    <FadingContent className="flex flex-col items-center text-center">
+      <p className="text-2xl font-montAlt">
+        Below are the things that should occur weekly barring any unforeseen
+        stuff. I'll update this when something with a schedule happens,
+        otherwise Twitter is where I'll announce anything that randomly comes up
+      </p>
+      <EventList
+        title="Recurring"
+        events={[
+          {
+            weekday: 'Sunday',
+            time: '1 PM EST',
+            name: 'Winds of Chaos, A Warhammer Fantasy 4e Game',
+            link: {
+              url: 'https://twitch.tv/welcomepartyrpg',
+              text: 'Welcome To The Party Twitch',
+            },
+          },
+        ]}
+      />
+    </FadingContent>
+  </Layout>
+)
 
 export default SchedulePage
